9b604fb8ec89cb86548777ab3e78e43ed0476e7b
jacquelineb <jacquelinesotheremail@gmail.com>
2022-08-10 00:59:01 -0700

    Moved difficulties list to constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const DIFFICULTIES = [
+  { level: 'beginner', numMines: 10, numRows: 9, numCols: 9 },
+  { level: 'intermediate', numMines: 40, numRows: 16, numCols: 16 },
+  { level: 'expert', numMines: 99, numRows: 16, numCols: 30 },
+];
+
+export { DIFFICULTIES };
