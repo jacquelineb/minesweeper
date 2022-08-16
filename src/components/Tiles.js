20ce41ffd58da927ab9a03b8e53f95cabe555bad
jacquelineb <jacquelinesotheremail@gmail.com>
2022-08-16 02:27:11 -0700

    Check that tiles not null/undefined before mapping

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -4,20 +4,21 @@ import Tile from './Tile';
 const Tiles = ({ tiles, onClickTile, onRightClickTile }) => {
   return (
     <>
-      {tiles.map((row, rowIndex) => {
-        return row.map((tile, colIndex) => {
-          return (
-            <Tile
-              key={`${rowIndex}-${colIndex}`}
-              revealTile={() => onClickTile(rowIndex, colIndex)}
-              flagTile={() => {
-                onRightClickTile(rowIndex, colIndex);
-              }}
-              tile={tile}
-            />
-          );
-        });
-      })}
+      {tiles &&
+        tiles.map((row, rowIndex) => {
+          return row.map((tile, colIndex) => {
+            return (
+              <Tile
+                key={`${rowIndex}-${colIndex}`}
+                revealTile={() => onClickTile(rowIndex, colIndex)}
+                flagTile={() => {
+                  onRightClickTile(rowIndex, colIndex);
+                }}
+                tile={tile}
+              />
+            );
+          });
+        })}
     </>
   );
 };
