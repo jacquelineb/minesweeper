9ea971328b20b9c14344d25bf4cf183c2b83ee12
jacquelineb <jacquelinesotheremail@gmail.com>
2022-08-16 02:32:58 -0700

    Moved game logic to useGame hook

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,45 +7,34 @@ import useTilesReducer from '../hooks/useTilesReducer';
 import explosion from '../assets/explosion.wav';
 import style from '../styles/Game.module.scss';
 
+import useGame from '../hooks/useGame';
+
 const Game = () => {
   const [difficulty, setDifficulty] = useState(DIFFICULTIES[0]);
-  const [potentialNumMinesLeft, setPotentialNumMinesLeft] = useState(difficulty.numMines);
-  const [gameStatus, setGameStatus] = useState(0); // win (1), lose (-1), or tbd (0)
-  const [tiles, tilesDispatch] = useTilesReducer([]);
+
+  const { tiles, gameStatus, numMinesRemaining, handleNewGame, handleReveal, handleFlag } =
+    useGame();
 
   const timerId = useRef();
   const timerIsActive = useRef(false);
   const [secondsLapsed, setSecondsLapsed] = useState(0);
 
   useEffect(() => {
-    resetGame();
+    const { numRows, numCols, numMines } = difficulty;
+    resetGame(numRows, numCols, numMines);
   }, [difficulty]);
 
   useEffect(() => {
-    if (tiles.length && gameStatus !== 'L') {
-      const gameIsWon = (() => {
-        for (const row of tiles) {
-          for (const tile of row) {
-            if (tile.isConcealed && tile.value !== 'M') {
-              return false;
-            }
-          }
-        }
-        return true;
-      })();
-
-      if (gameIsWon) {
-        setGameStatus('W');
-        stopTimer();
-      }
-    }
-  }, [tiles, gameStatus]);
-
-  useEffect(() => {
-    if (gameStatus === 'W') {
-      tilesDispatch({ type: 'FLAG_ALL_MINES' });
+    if (gameStatus) {
+      stopTimer();
     }
   }, [gameStatus]);
+
+  const resetGame = () => {
+    handleNewGame(difficulty.numRows, difficulty.numCols, difficulty.numMines);
+    stopTimer();
+    setSecondsLapsed(0);
+  };
 
   const startTimer = () => {
     timerId.current = setInterval(() => {
@@ -59,54 +48,12 @@ const Game = () => {
     timerIsActive.current = false;
   };
 
-  const resetGame = () => {
-    tilesDispatch({ type: 'INITIALIZE_TILES', payload: { ...difficulty } });
-    setGameStatus(0);
-    setPotentialNumMinesLeft(difficulty.numMines);
-    stopTimer();
-    setSecondsLapsed(0);
-  };
-
   const handleClick = (row, col) => {
     if (!timerIsActive.current) {
       timerIsActive.current = true;
       startTimer();
     }
-
-    if (tiles[row][col].isConcealed && !tiles[row][col].isFlagged) {
-      if (tiles[row][col].value === 'M') {
-        const audio = new Audio(explosion);
-        audio.play();
-        setGameStatus('L');
-        stopTimer();
-
-        tilesDispatch({
-          type: 'EXPLODE_MINE',
-          payload: { tileCoord: { row, col } },
-        });
-        tilesDispatch({ type: 'REVEAL_MINES_AND_FLAGS' });
-      } else {
-        tilesDispatch({
-          type: 'REVEAL_SAFE_AREA',
-          payload: { tileCoord: { row, col } },
-        });
-      }
-    }
-  };
-
-  const handleRightClick = (row, col) => {
-    if (tiles[row][col].isConcealed) {
-      if (!tiles[row][col].isFlagged) {
-        tilesDispatch({
-          type: 'FLAG_TILE',
-          payload: { tileCoord: { row, col } },
-        });
-        setPotentialNumMinesLeft(potentialNumMinesLeft - 1);
-      } else {
-        tilesDispatch({ type: 'UNFLAG_TILE', payload: { tileCoord: { row, col } } });
-        setPotentialNumMinesLeft(potentialNumMinesLeft + 1);
-      }
-    }
+    handleReveal(row, col);
   };
 
   const handleDifficultyChange = (newDifficulty) => {
@@ -114,33 +61,20 @@ const Game = () => {
   };
 
   return (
-    <>
-      <div className={style.container}>
-        <DifficultyMenu currDifficulty={difficulty} selectDifficulty={handleDifficultyChange} />
-        <Display
-          time={secondsLapsed}
-          numMines={potentialNumMinesLeft}
-          status={gameStatus}
-          onClick={resetGame}
-        />
+    <div className={style.container} onContextMenu={(e) => e.preventDefault()}>
+      <DifficultyMenu currDifficulty={difficulty} selectDifficulty={handleDifficultyChange} />
 
-        <div className={`${style.tilesContainer} ${style[difficulty.level]}`}>
-          <Tiles
-            tiles={tiles}
-            onClickTile={(row, col) => {
-              if (!gameStatus) {
-                handleClick(row, col);
-              }
-            }}
-            onRightClickTile={(row, col) => {
-              if (!gameStatus) {
-                handleRightClick(row, col);
-              }
-            }}
-          />
-        </div>
+      <Display
+        time={secondsLapsed}
+        numMines={numMinesRemaining}
+        status={gameStatus}
+        onClick={resetGame}
+      />
+
+      <div className={`${style.tilesContainer} ${style[difficulty.level]}`}>
+        <Tiles tiles={tiles} onClickTile={handleClick} onRightClickTile={handleFlag} />
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ src/hooks/useGame.js
@@ -0,0 +1,130 @@
+import { useEffect, useState } from 'react';
+import createTiles from '../utils/createTiles';
+import explosionSound from '../assets/explosion.wav';
+import flagSound from '../assets/flag.ogg';
+import unflagSound from '../assets/unflag.ogg';
+
+const useGame = () => {
+  const [tiles, setTiles] = useState();
+  const [gameStatus, setGameStatus] = useState(0);
+  const [numMinesRemaining, setNumMinesRemaining] = useState();
+
+  // Check if game is won or is still going
+  useEffect(() => {
+    if (tiles && gameStatus !== 'L') {
+      const gameIsWon = (() => {
+        for (const row of tiles) {
+          for (const tile of row) {
+            if (tile.isConcealed && tile.value !== 'M') {
+              return false;
+            }
+          }
+        }
+        return true;
+      })();
+
+      if (gameIsWon) {
+        setGameStatus('W');
+      }
+    }
+  }, [tiles]);
+
+  useEffect(() => {
+    // When game is won, flag all the mines if not already flagged
+    if (gameStatus === 'W') {
+      setTiles((prevTiles) => {
+        return prevTiles.map((rowOfTiles) => {
+          return rowOfTiles.map((tile) => {
+            if (tile.value === 'M' && !tile.isFlagged) {
+              return { ...tile, isFlagged: true };
+            }
+            return tile;
+          });
+        });
+      });
+    }
+  }, [gameStatus]);
+
+  const handleNewGame = (numRows, numCols, numMines) => {
+    const newTiles = createTiles(numRows, numCols, numMines);
+    setTiles(newTiles);
+    setNumMinesRemaining(numMines);
+    setGameStatus(0);
+  };
+
+  const handleReveal = (row, col) => {
+    if (!gameStatus && tiles[row][col].isConcealed && !tiles[row][col].isFlagged) {
+      const updatedTiles = tiles.map((rowOfTiles) => rowOfTiles.map((tile) => tile));
+
+      // Clicking on a mine. Mine explodes (set its value to 'X') and reveal all mines and flagged tiles)
+      if (tiles[row][col].value === 'M') {
+        setGameStatus('L');
+        updatedTiles.forEach((rowOfTiles, i) => {
+          rowOfTiles.forEach((tile, j) => {
+            if (tile.value === 'M' || tile.isFlagged) {
+              const updatedTile = { ...tile, isConcealed: false };
+              if (i === row && j === col) {
+                updatedTile.value = 'X';
+                const audio = new Audio(explosionSound);
+                audio.play();
+              }
+              updatedTiles[i][j] = updatedTile;
+            }
+          });
+        });
+      }
+      // Clicking on non-mine tile. If the tile has 0 mines around it, recursively repeat the process to reveal non-mine tiles around it.
+      else {
+        (function revealSafeArea(row, col) {
+          updatedTiles[row][col] = { ...tiles[row][col], isConcealed: false };
+          if (updatedTiles[row][col].value === 0) {
+            for (let i = row - 1; i <= row + 1; i++) {
+              for (let j = col - 1; j <= col + 1; j++) {
+                if (
+                  !(i === row && j === col) &&
+                  updatedTiles[i] &&
+                  updatedTiles[i][j] &&
+                  updatedTiles[i][j].isConcealed &&
+                  !updatedTiles[i][j].isFlagged &&
+                  updatedTiles[i][j].value !== 'M'
+                ) {
+                  revealSafeArea(i, j);
+                }
+              }
+            }
+          }
+        })(row, col);
+      }
+      setTiles(updatedTiles);
+    }
+  }; // handleReveal
+
+  const handleFlag = (row, col) => {
+    if (!gameStatus && tiles[row][col].isConcealed) {
+      const updatedTiles = tiles.map((rowOfTiles) => rowOfTiles.map((tile) => tile));
+      if (!tiles[row][col].isFlagged) {
+        setNumMinesRemaining((prevState) => prevState - 1);
+        updatedTiles[row][col] = { ...tiles[row][col], isFlagged: true };
+        const audio = new Audio(flagSound);
+        audio.play();
+      } else {
+        setNumMinesRemaining((prevState) => prevState + 1);
+        updatedTiles[row][col] = { ...tiles[row][col], isFlagged: false };
+        const audio = new Audio(unflagSound);
+        audio.play();
+      }
+      setTiles(updatedTiles);
+    }
+  }; // handleFlag
+
+  return {
+    tiles,
+    gameStatus,
+    numMinesRemaining,
+    handleNewGame,
+    handleReveal,
+    handleFlag,
+  };
+};
+
+export default useGame;
